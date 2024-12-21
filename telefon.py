6af4a128d2725a7cb4f2a2707dadf69d60369f08
MachBB <dmt.medrzak@lo5.bielsko.pl>
2024-12-21 12:06:07 +0100

    Add files via upload

--- telefon.py
+++ telefon.py
@@ -1,1 +1,22 @@
+'''def sprawdz_numer_telefonu(numer):
+    if len(numer) == 9 and numer.isdigit():
+        return "Numer telefonu jest poprawny."
+    else:
+        return "Numer telefonu jest niepoprawny."
 
+numer = input("Podaj numer telefonu: ")
+print(sprawdz_numer_telefonu(numer))'''
+
+import re
+
+def sprawdz_numer_telefonu(tel):
+
+    wzorzec = r'^\d{9}$'
+    
+    if re.match(wzorzec, tel):
+        return "Numer telefonu jest poprawny."
+    else:
+        return "Numer telefonu jest niepoprawny."
+
+tel = input("Podaj numer telefonu: ")
+print(sprawdz_numer_telefonu(tel))
